52d48baa6ef2ffc799b9f73d69656f82bbb9c766
1129hiki <54256847+1129hiki@users.noreply.github.com>
2022-03-26 16:47:49 +0000

    Update 123_betting.py
    modified calculation of budget

--- 123_betting.py
+++ 123_betting.py
@@ -72,7 +72,7 @@ def do_roulette(unit, budget, multiplier):
     result_string = "win" if result == 1 else "lose"
 
     if result == 1 :
-      budget += multiplier * bet
+      budget += (multiplier - 1) * bet
       profit = budget - initial_budget
 
       progression = progression[(multiplier - 1): -(multiplier - 1)]
